05a79e2a9c42249af29b526750e3f38d26937ad9
himangshudeepkalita <itzhimangshu@gmail.com>
2025-08-07 22:15:58 +0530

    fixed weather component on reload

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -6,6 +6,7 @@ const weatherApiKey = import.meta.env.VITE_WEATHER_API_KEY;
 const Weather = () => {
   const [data, setData] = useState(null);
   // const [city, setCity] = useState("");
+  const [city, setCity] = useState(null);
 
   // useEffect(() => {
   //   const savedCity = localStorage.getItem("userCity");
@@ -15,25 +16,47 @@ const Weather = () => {
   // }, []);
 
   useEffect(() => {
+    const checkCity = () => {
+      const savedCity = localStorage.getItem("userCity");
+      if (savedCity) {
+        setCity(savedCity);
+        return true;
+      }
+      return false;
+    };
+
+    if (checkCity()) return;
+
+    const interval = setInterval(() => {
+      if (checkCity()) clearInterval(interval);
+    }, 300);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
     // if (!city) return;
 
     // const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${weatherApiKey}`;
 
-    const savedCity = localStorage.getItem("userCity");
+    if (!city) return;
 
-    if (savedCity) {
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${savedCity}&units=metric&appid=${weatherApiKey}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${weatherApiKey}`;
 
-      axios
-        .get(url)
-        .then((response) => {
-          setData(response.data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    }
-  }, []);
+    // const savedCity = localStorage.getItem("userCity");
+
+    // if (savedCity) {
+    //   const url = `https://api.openweathermap.org/data/2.5/weather?q=${savedCity}&units=metric&appid=${weatherApiKey}`;
+
+    axios
+      .get(url)
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [city]);
 
   return (
     <div className="weather-container">
